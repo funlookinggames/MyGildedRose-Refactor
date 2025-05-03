1bebfdde56f8dce0643b1bea5f393a90cf6a14f6
funlookinggames <afonso.junior9522@gmail.com>
2025-05-03 13:36:59 -0300

    GildedRose.cs aplicação dos requisitos pra items conjurados

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -18,11 +18,11 @@ public class GildedRose
             bool isAgedBrie = I.Name == "Aged Brie";
             bool isBackStage = I.Name == "Backstage passes to a TAFKAL80ETC concert";
             bool isSulfuras = I.Name == "Sulfuras, Hand of Ragnaros";
+            bool isConjured = I.Conjured == true;
 
-
-            if (!isAgedBrie && !isBackStage)
+            /*if (!isAgedBrie && !isBackStage)
             {
-                if (I.Quality > 0 && !isSulfuras) I.Quality--;
+                if (I.Quality > 0 && !isSulfuras && I.SellIn > 0) I.Quality--;
             }
             else
             {
@@ -40,6 +40,8 @@ public class GildedRose
             }
             // MAKE CONJURED ITEMS TAKE DOUBLE DAMAGE IN QUALITY LOL
 
+            if (isConjured && !isSulfuras && I.Quality > 0) { I.Quality--; }
+            */
             // if (!isSulfuras) I.SellIn--;
 
             if (I.SellIn < 0)
@@ -59,10 +61,49 @@ public class GildedRose
                         break;
 
                     default:
-                        if (I.Quality > 1) I.Quality-=2;
-                        else I.Quality = 0;
-                            break;
+                        if (isConjured)
+                        {
+                            if (I.Quality > 1) I.Quality -= 2;
+
+                        } else
+                        {
+                            if (I.Quality > 3) I.Quality -= 4;
+                            else I.Quality = 0;
+                        }
+                        break;
                 }
+            }
+            else
+            {
+                // meaning, if sellin > 0;
+                switch (I.Name)
+                {
+                    case "Aged Brie":
+                        if (I.Quality > 0) I.Quality++;
+
+                        break;
+                    case "Backstage passes to a TAFKAL80ETC concert":
+
+                        if (I.Quality > 0) I.Quality++;
+
+                        if (I.SellIn <= 10 && I.Quality < 50) I.Quality++;
+
+                        if (I.SellIn <= 5 && I.Quality < 50) I.Quality++;
+
+                        break;
+                    case "Sulfuras, Hand of Ragnaros":
+                        // Do nothing;
+                        break;
+                    default:
+                        if (I.Quality > 0)
+                        {
+                            I.Quality--;
+
+                            if (isConjured) I.Quality--;
+                        }
+                        break;
+                }
+                if (!isSulfuras) I.SellIn--;
             }
         }
     }
--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -9,7 +9,7 @@ public class GildedRoseTest
     [Test]
     public void Foo()
     {
-        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0, Conjured = false } };
         var app = new GildedRose(items);
         app.UpdateQuality();
         Assert.That(items[0].Name, Is.EqualTo("foo"));
